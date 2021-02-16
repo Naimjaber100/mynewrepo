0c2d89d7108524134e58ebeaf212e5c501c9972f
naim jaber <naim.jaber100@gmail.com>
2021-02-16 10:08:55 +0200

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using language1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace language1.Controllers
+{
+    public class HomeController : MyController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Registration r)
+        {
+            return View(r);
+        }
+
+
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new LanguageMang().SetLanguage(lang);
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
